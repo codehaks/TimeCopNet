fd0f89178dadb8713b723dee427b3c91bcc3b69b
Hakim Ghods <ghods.hakim@hotmail.com>
2022-07-26 22:50:29 +0430

    feat: bootstrap UI

--- src/TimeCop.Web/Program.cs
+++ src/TimeCop.Web/Program.cs
@@ -1,13 +1,44 @@
+using Codehaks.Identity.Data;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using TimeCop.Identity.Models;
 using TimeCop.TimeSheet;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<UserDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("Identity")));
+
 builder.Services.AddDbContext<TimeSheetDbContext>
     (options => options.UseNpgsql(builder.Configuration.GetConnectionString("TimeSheet")));
 
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = false)
+    //.AddDefaultUI()
+    .AddSignInManager()
+    .AddEntityFrameworkStores<UserDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddRazorPages();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseStatusCodePagesWithReExecute("/errors/{0}");
+    app.UseExceptionHandler("/error");
+}
+else
+{
+    app.UseStatusCodePagesWithReExecute("/errors/{0}");
+    app.UseExceptionHandler("/error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapRazorPages();
 
 app.Run();
