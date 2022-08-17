0d30bc6f46f8eb0972c790adc42f268b26850a58
Hakim Ghods <ghods.hakim@hotmail.com>
2022-08-17 16:59:29 +0430

    imp: landig page

---
+++ src/TimeCop.Web/Areas/User/Pages/Jobs/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TimeCop.Web.Areas.User.Pages.Jobs;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
