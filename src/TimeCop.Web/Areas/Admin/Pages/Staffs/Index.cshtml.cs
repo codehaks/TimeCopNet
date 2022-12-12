66a212f097f61125a9e7dcc4be6dfb83bc6f84be
Hakim Ghods <ghods.hakim@hotmail.com>
2022-12-12 11:20:09 +0330

    Staff Service #2

---
+++ src/TimeCop.Web/Areas/Admin/Pages/Staffs/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TimeCop.Web.Areas.Admin.Pages.Staffs
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
