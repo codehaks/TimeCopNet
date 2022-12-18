de5cb4a4e21b042a413998f3a97c6c9b4480a782
Hakim Ghods <ghods.hakim@hotmail.com>
2022-12-18 14:49:42 +0330

    fix

--- src/TimeCop.TimeSheet/Domain/Hour.cs
+++ src/TimeCop.TimeSheet/Domain/Hour.cs
@@ -5,15 +5,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TimeCop.TimeSheet.Domain
+namespace TimeCop.TimeSheet.Domain;
+
+public class Hour
 {
-    public class Hour
-    {
-        public int Id { get; set; }
-        public required string StaffName { get; set; }
-        public required string StaffId { get; set; }
-        public required LocalTime LogTime { get; set; }
-        public required string Status { get; set; }
-        public string? Note { get; set; }
-    }
+
+    public int Id { get; set; }
+    public required string StaffName { get; set; }
+    public required string StaffId { get; set; }
+    public required LocalDateTime LogTime { get; set; }
+    public required string Status { get; set; }
+    public string? Note { get; set; }
 }
---
+++ src/TimeCop.TimeSheet/Domain/Session.cs
@@ -0,0 +1,63 @@
+﻿using NodaTime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeCop.TimeSheet.Domain;
+
+public enum SessionState
+{
+    Todo=0,
+    InProgress=1,
+    Done=2
+}
+
+public class Session
+{
+    private readonly string StaffId;
+    private readonly string StaffName;
+
+    public Session(string staffId, string staffName)
+    {
+        StaffId = staffId;
+        StaffName = staffName;
+    }
+
+    public Hour? StartHour { get; private set; }
+    public Hour? EndHour { get; private set; }
+
+    public void Start(string note = "")
+    {
+
+        StartHour = new Hour
+        {
+            LogTime = LocalDateTime.FromDateTime(DateTime.Now),
+            StaffId = StaffId,
+            StaffName = StaffName,
+            Status = "in"
+        };
+    }
+
+    public void End(Hour hour)
+    {
+        if (StartHour is not null && hour.LogTime > StartHour.LogTime)
+        {
+            EndHour = new Hour
+            {
+                LogTime = LocalDateTime.FromDateTime(DateTime.Now),
+                StaffId = StaffId,
+                StaffName = StaffName,
+                Note=StartHour.Note,
+                Status = "out"
+            };
+        }
+
+    }
+
+    public Period GetLength()
+    {
+        return EndHour.LogTime - StartHour.LogTime;
+    }
+}
